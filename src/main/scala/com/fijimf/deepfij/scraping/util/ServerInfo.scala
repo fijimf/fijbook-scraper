7f1faff325e657ad998321d6966171302ec48e0c
Jim Frohnhofer <jfrohnhofer@kabbage.com>
2020-03-14 00:29:15 -0400

    Fixed services reporting

--- src/main/scala/com/fijimf/deepfij/scraping/util/ServerInfo.scala
+++ src/main/scala/com/fijimf/deepfij/scraping/util/ServerInfo.scala
@@ -3,7 +3,6 @@ package com.fijimf.deepfij.scraping.util
 import cats.Applicative
 import com.fijimf.deepfij.scraping.BuildInfo
 import io.circe.Encoder
-import io.circe.generic.semiauto.deriveEncoder
 import org.http4s.EntityEncoder
 import org.http4s.circe.jsonEncoderOf
 
@@ -13,10 +12,14 @@ case class ServerInfo(name: String, version: String, scalaVersion: String, sbtVe
 
 case object ServerInfo {
 
-  implicit val healthyEncoder: Encoder.AsObject[ServerInfo] = deriveEncoder[ServerInfo]
+  implicit val healthyEncoder: Encoder.AsObject[ServerInfo] =
+    Encoder.forProduct7("name", "version", "scalaVersion", "sbtVersion", "buildNumber", "builtAt", "isOk")(u =>
+      (u.name, u.version, u.scalaVersion, u.sbtVersion, u.buildNumber, u.builtAt, u.isOk)
+    )
+
   implicit def healthyEntityEncoder[F[_] : Applicative]: EntityEncoder[F, ServerInfo] = jsonEncoderOf
 
-  def fromStatus(status:Map[String,Boolean]): ServerInfo = {
+  def fromStatus(status: Map[String, Boolean]): ServerInfo = {
     ServerInfo(
       BuildInfo.name,
       BuildInfo.version,
